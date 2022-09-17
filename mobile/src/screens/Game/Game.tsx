ed6ea9c7b24b6678df8df92849810a37216a79de
mamede <felipebrasilmv@gmail.com>
2022-09-17 17:24:13 -0300

    feat: aula 05 mobile

--- mobile/src/screens/Game/Game.tsx
+++ mobile/src/screens/Game/Game.tsx
@@ -14,9 +14,11 @@ import { GameParams } from "../../@types/navigation";
 import { Heading } from "../../components/Heading/Heading";
 import { Background } from "../../components/Background/Background";
 import { DuoCard, DuoCardProps } from "../../components/DuoCard/DuoCard";
+import { DuoMatch } from '../../components/DuoMatch/DuoMatch';
 
 export function Game() {
   const [duos, setDuos] = useState<DuoCardProps[]>([])
+  const [discordDuoSelected, setDiscordDuoSelected] = useState('')
 
   const navigation = useNavigation();
   const router = useRoute();
@@ -24,6 +26,12 @@ export function Game() {
 
   function handleGoBack() {
     navigation.goBack();
+  }
+
+  async function getDiscordUser(adsId: string) {
+    fetch(`http://192.168.0.233:3333/ads/${adsId}/discord`)
+      .then(res => res.json())
+      .then(data => setDiscordDuoSelected(data.discord));
   }
 
   useEffect(() => {
@@ -69,7 +77,7 @@ export function Game() {
           renderItem={({ item }) => (
             <DuoCard
               data={item}
-              onConnect={() => {}}
+              onConnect={() => getDiscordUser(item.id)}
             />
           )}
           horizontal
@@ -81,6 +89,12 @@ export function Game() {
               Não há anúncios publicados ainda.
             </Text>
           }
+        />
+        
+        <DuoMatch
+          visible={discordDuoSelected.length > 0}
+          discord={discordDuoSelected}
+          onClose={() => setDiscordDuoSelected('')}
         />
       </SafeAreaView>
     </Background>
